24f79a8a236f9e79a58c15fa866f0ece58bc9fbe
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 22:48:54 +0100

    feat: implement tf-atom-sns-topic-aws module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,19 @@
 output "enabled" {
-  description = "Whether the module is enabled."
+  description = "Whether the module is enabled"
   value       = local.enabled
+}
+
+output "arn" {
+  description = "ARN of the SNS topic"
+  value       = try(aws_sns_topic.this[0].arn, null)
+}
+
+output "id" {
+  description = "ARN of the SNS topic"
+  value       = try(aws_sns_topic.this[0].id, null)
+}
+
+output "name" {
+  description = "Name of the SNS topic"
+  value       = try(aws_sns_topic.this[0].name, null)
 }
